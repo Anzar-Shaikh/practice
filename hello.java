47ffaa009a1354c90b1251ed4f192714f3a33291
Anzar Shaikh <anzarshaikh373@gmail.com>
2021-06-30 09:48:12 +0530

    hello done practice done

---
+++ hello.java
@@ -0,0 +1,5 @@
+package com.bl.corejava;
+
+public class hello {
+
+}
--- practice.java
+++ practice.java
@@ -3,5 +3,6 @@ package com.bl.corejava;
 public class practice {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("New thing");
     }
 }
